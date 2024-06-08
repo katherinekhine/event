7da9c04119e264b4ec7dba604ad07440e2a94f97
katherinekhine <khaingkatherine@gmail.com>
2024-06-08 21:09:14 +0630

    update event.php file

--- event.php
+++ event.php
@@ -13,9 +13,9 @@ include "components/header.php";
 
 <div class="mt-5 lg:px-20 px-5">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        <div class="flex flex-col justify-center items-center px-3 border border-color-primary">
+        <div class="flex flex-col justify-center items-center px-3 border border-color-primary hover:border-color-primary hover:shadow-lg hover:shadow-color-primary">
             <div class="py-2">
-                <img src="img/home.png" alt="" class="rounded-md hover:scale-105 ease-linear duration-200 ">
+                <img src="img/event-1.png" alt="" class="rounded-md hover:scale-105 ease-linear duration-200">
             </div>
             <div>
                 <h3 class="h3 text-color-primary text-center">Artistic Adventures</h3>
@@ -25,37 +25,37 @@ include "components/header.php";
                 <button class="btn btn-primary"><a href="#"></a>Join</button>
             </div>
         </div>
-        <div class="flex flex-col justify-center items-center px-3 border border-color-primary">
+        <div class="flex flex-col justify-center items-center px-3 border border-color-primary hover:border-color-primary hover:shadow-lg hover:shadow-color-primary">
             <div class="py-2">
-                <img src="img/home.png" alt="" class="rounded-md hover:scale-105 ease-linear duration-200 ">
+                <img src="img/event-2.png" alt="" class="rounded-md hover:scale-105 ease-linear duration-200 ">
             </div>
             <div>
-                <h3 class="h3 text-color-primary text-center">Artistic Adventures</h3>
-                <p class="p mt-2">Discover a myriad of creative experiences with our partnered Art Club. Explore captivating art exhibitions, attend hands-on workshops, and engage with local artists as they showcase their talents. Whether you're a seasoned artist or simply appreciate the beauty of art, our curated selection of art events offers something for everyone to enjoy.</p>
+                <h3 class="h3 text-color-primary text-center">Music Club</h3>
+                <p class="p mt-2">Feel the rhythm and connect with fellow music lovers through Music Club. This club is a haven for musicians and enthusiasts alike, offering jam sessions, concerts, and lively discussions about various genres and artists. Whether you play an instrument or simply enjoy listening, the Music Club welcomes you to join their community.</p>
             </div>
             <div class="my-4">
                 <button class="btn btn-primary"><a href="#"></a>Join</button>
             </div>
         </div>
-        <div class="flex flex-col justify-center items-center px-3 border border-color-primary">
+        <div class="flex flex-col justify-center items-center px-3 border border-color-primary hover:border-color-primary hover:shadow-lg hover:shadow-color-primary">
             <div class="py-2">
-                <img src="img/home.png" alt="" class="rounded-md hover:scale-105 ease-linear duration-200 ">
+                <img src="img/event-3.png" alt="" class="rounded-md hover:scale-105 ease-linear duration-200 ">
             </div>
             <div>
-                <h3 class="h3 text-color-primary text-center">Artistic Adventures</h3>
-                <p class="p mt-2">Discover a myriad of creative experiences with our partnered Art Club. Explore captivating art exhibitions, attend hands-on workshops, and engage with local artists as they showcase their talents. Whether you're a seasoned artist or simply appreciate the beauty of art, our curated selection of art events offers something for everyone to enjoy.</p>
+                <h3 class="h3 text-color-primary text-center">English Community Club</h3>
+                <p class="p mt-2">Enhance your language skills and make new friends by joining our partnered English Community Club. Ideal for both native speakers and learners, this club hosts a variety of activities including book discussions, conversation groups, and writing workshops. Immerse yourself in the English language and culture while building meaningful connections.</p>
             </div>
             <div class="my-4">
                 <button class="btn btn-primary"><a href="#"></a>Join</button>
             </div>
         </div>
-        <div class="flex flex-col justify-center items-center px-3 border border-color-primary">
+        <div class="flex flex-col justify-center items-center px-3 border border-color-primary hover:border-color-primary hover:shadow-lg hover:shadow-color-primary">
             <div class="py-2">
-                <img src="img/home.png" alt="" class="rounded-md hover:scale-105 ease-linear duration-200 ">
+                <img src="img/event-4.png" alt="" class="rounded-md hover:scale-105 ease-linear duration-200 ">
             </div>
             <div>
-                <h3 class="h3 text-color-primary text-center">Artistic Adventures</h3>
-                <p class="p mt-2">Discover a myriad of creative experiences with our partnered Art Club. Explore captivating art exhibitions, attend hands-on workshops, and engage with local artists as they showcase their talents. Whether you're a seasoned artist or simply appreciate the beauty of art, our curated selection of art events offers something for everyone to enjoy.</p>
+                <h3 class="h3 text-color-primary text-center">Engaging Book Discussions</h3>
+                <p class="p mt-2">Dive deep into captivating stories and thought-provoking narratives with regular discussion meetings. Each session focuses on a different book, and offering members the opportunity to share their thoughts. Engaging in these discussions not only enhances your understanding of the text but also allows you to appreciate diverse viewpoints.</p>
             </div>
             <div class="my-4">
                 <button class="btn btn-primary"><a href="#"></a>Join</button>
