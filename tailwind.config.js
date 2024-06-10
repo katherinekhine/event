d3d30c97de59be393006a9a33ec45eaed0567e1b
Arkar Min <25-00286@dat.com>
2024-06-10 13:22:12 +0630

    Add footer component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js,php}", "./js/**/*.js", "./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js,php}", "./js/**/*.js", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
