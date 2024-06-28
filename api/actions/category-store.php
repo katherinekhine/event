7ed35452829e6c95e7308ad7dea2b9ab4413e1a6
katherinekhine <khaingkatherine@gmail.com>
2024-06-28 14:39:26 +0630

    update category-store.php and Category.php

--- api/actions/category-store.php
+++ api/actions/category-store.php
@@ -4,7 +4,26 @@ include "../../vendor/autoload.php";
 
 use api\classes\Category;
 
+$event_id = $_GET['id'];
 $detail = htmlspecialchars(trim($_POST['detail']));
 $time = htmlspecialchars(trim($_POST['time']));
 $location = htmlspecialchars(trim($_POST['location']));
 $img = $_FILES['img'];
+
+if (empty($event_id) || empty($detail) || empty($time) || empty($location) || !is_uploaded_file($img['tmp_name'])) {
+    header('location: ../category-create.php?error=1');
+} else {
+    $img_path = "photos/category" . $img['name'];
+    move_uploaded_file($img['tmp_name'], "../../" . $img_path);
+
+    $category = new Category();
+    $category->catStore([
+        'event_id' => $event_id,
+        'detail' => $detail,
+        'time' => $time,
+        'location' => $location,
+        'img' => $img_path,
+    ]);
+
+    header("location: ../admin-category.php");
+}
--- api/classes/Category.php
+++ api/classes/Category.php
@@ -19,4 +19,44 @@ class Category extends Database
         ]);
         return $result->rowCount();
     }
+
+    public function catAll()
+    {
+        $query = "SELECT * FROM category";
+        $result = $this->connect()->prepare($query);
+        $result->execute();
+        return $result->fetchAll();
+    }
+
+    public function catShow($id)
+    {
+        $query = "SELECT * FROM category WHERE id = :id";
+        $result = $this->connect()->prepare($query);
+        $result->execute([
+            ':id' => $id
+        ]);
+        return $result->fetchObject();
+    }
+
+    public function catUpdate($data)
+    {
+        if ($data['img']) {
+            $query = "UPDATE category SET detail = :detail, time = :time, location = :location, img = :img WHERE id = :id";
+        } else {
+            $query = "UPDATE category SET detail = :detail, time = :time, location = :location WHERE id = :id";
+        }
+        $result = $this->connect()->prepare($query);
+        $result->execute($data);
+        return $result->rowCount();
+    }
+
+    public function catDelete($id)
+    {
+        $query = "DELETE FROM category WHERE id = :id";
+        $result = $this->connect()->prepare($query);
+        $result->execute([
+            ':id' => $id
+        ]);
+        return $result->rowCount();
+    }
 }
