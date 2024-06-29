6acac2c3f4509a2b3067fe4268e2fdd6f1147c8f
katherinekhine <khaingkatherine@gmail.com>
2024-06-29 21:53:03 +0630

    update category-update.php file

--- api/actions/category-update.php
+++ api/actions/category-update.php
@@ -3,3 +3,33 @@
 include "../../vendor/autoload.php";
 
 use api\classes\Category;
+
+
+$detail = htmlspecialchars(trim($_POST['detail']));
+$time = htmlspecialchars(trim($_POST['time']));
+$location = htmlspecialchars(trim($_POST['location']));
+$img = $_FILES['img'];
+
+if (empty($detail) || empty($time) || empty($location)) {
+    header("location:../category-edit.php?error=1");
+} else {
+    $category = new Category();
+
+    if (is_uploaded_file($img['tmp_name'])) {
+        $img_path = "photos/category" . $img['name'];
+        move_uploaded_file($img['tmp_name'], "../../" . $img_path);
+        $category->catUpdate([
+            'detail' => $detail,
+            'time' => $time,
+            'location' => $location,
+            'img' => $img_path,
+        ]);
+    } else {
+        $category->catUpdate([
+            'detail' => $detail,
+            'time' => $time,
+            'location' => $location,
+        ]);
+    }
+    header("location: ../admin-category.php");
+}
